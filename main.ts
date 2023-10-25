b94c284507d5c52b4003b6c67cf26b94a53387d4
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-25 12:33:27 -0400

    ???

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Kyle Lyver
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+// screen setup
+basic.showIcon(IconNames.Happy)
+
+// get distance from sonar
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showNumber(distanceToObject)
+  basic.showIcon(IconNames.Happy)
+})
